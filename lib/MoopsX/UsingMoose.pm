2caed6a379593ec958da3d2153d1f246aaebf4cc
Erik Carlsson <info@code301.com>
2015-03-16 17:53:47 +0100

    Init

---
+++ lib/MoopsX/UsingMoose.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package MoopsX::UsingMoose;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use MoopsX::UsingMoose;
+
+=head1 DESCRIPTION
+
+MoopsX::UsingMoose is ...
+
+=head1 SEE ALSO
+
+=cut
